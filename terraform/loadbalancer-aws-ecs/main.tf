944ce168ec67dcee7d7f25f4059cfcf21a49660d
Gavin Purcell <mkorangestripe@gmail.com>
2021-04-10 15:39:57 -0500

    Add aws_route_table, add ingress ports 22 and 443 to aws_security_group, remove network interface.

--- terraform/loadbalancer-aws-ecs/main.tf
+++ terraform/loadbalancer-aws-ecs/main.tf
@@ -4,6 +4,8 @@ provider "aws" {
 
 resource "aws_vpc" "loadbalancer-app2" {
   cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
     tags = {
     Name = "ECS loadbalancer-app2 - VPC"
@@ -17,6 +19,23 @@ resource "aws_subnet" "loadbalancer-app2" {
     tags = {
     Name = "ECS loadbalancer-app2 - Public Subnet"
   }
+}
+
+resource "aws_route_table" "loadbalancer-app2" {
+  vpc_id = aws_vpc.loadbalancer-app2.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.loadbalancer-app2.id
+  }
+    tags = {
+    Name = "ECS loadbalancer-app2 - RouteTable"
+  }
+}
+
+resource "aws_route_table_association" "loadbalancer-app2" {
+  subnet_id = aws_subnet.loadbalancer-app2.id
+  route_table_id = aws_route_table.loadbalancer-app2.id
 }
 
 resource "aws_internet_gateway" "loadbalancer-app2" {
@@ -83,12 +102,27 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
 resource "aws_security_group" "loadbalancer-app2-security-group" {
   name       = "ECS loadbalancer-app2 - ECS SecurityGroup"
   vpc_id     = aws_vpc.loadbalancer-app2.id
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.loadbalancer-app2.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port    = 443
+    to_port      = 443
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+}
 
   egress {
     from_port   = 0
@@ -100,7 +134,7 @@ resource "aws_security_group" "loadbalancer-app2-security-group" {
 
 resource "aws_ecs_service" "loadbalancer-app2" {
   name            = "loadbalancer-app2"                              # service name
-  cluster         = aws_ecs_cluster.loadbalancer-app2.name           # referencing the created cluster
+  cluster         = aws_ecs_cluster.loadbalancer-app2.id             # referencing the created cluster
   task_definition = aws_ecs_task_definition.loadbalancer-app2.arn    # referencing the task the service will start
   launch_type     = "FARGATE"
   desired_count   = 1 # the number of containers to deploy
@@ -110,15 +144,4 @@ resource "aws_ecs_service" "loadbalancer-app2" {
     assign_public_ip = true # Providing our containers with public IPs
     security_groups  = [aws_security_group.loadbalancer-app2-security-group.id] # Setting the security group
   }
-}
-
-resource "aws_network_interface" "loadbalancer-app2" {
-  subnet_id       = aws_subnet.loadbalancer-app2.id
-  # private_ips     = ["10.0.0.50"]
-  security_groups = [aws_security_group.loadbalancer-app2-security-group.id]
-
-#   attachment {
-#     instance     = 
-#     device_index = 1
-#   }
 }
