8aac61bbd061102ff4f4b59a367ad589f21b7d97
Gavin Purcell <mkorangestripe@gmail.com>
2021-08-19 13:50:14 -0500

    Update to handle multiple lines returned from the host command.

--- networking/chkdns.sh
+++ networking/chkdns.sh
@@ -6,19 +6,20 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-ARECORD=$(host "$1") || { echo $ARECORD; exit 1; }
-ARECORD_HOSTNAME=$(echo $ARECORD | awk '{print $1}')
-ARECORD_IPADDR=$(echo $ARECORD | awk '{printf $4}')
-PTR=$(host $ARECORD_IPADDR)
-PTR_HOSTNAME=$(echo $PTR | awk '{printf $5}')
+DNS_RETURN=$(host "$1") || { echo "$DNS_RETURN"; exit 1; }
+ARECORD_HOSTNAME=$(echo "$DNS_RETURN" | head -1 | awk '{print $1}')
+ARECORD_IPADDR=$(echo "$DNS_RETURN" | head -1 | awk '{printf $4}')
 
-echo $ARECORD
-echo $PTR
+PTR=$(host "$ARECORD_IPADDR") || { echo "$PTR"; exit 1; }
+PTR_HOSTNAME=$(echo "$PTR" | awk '{printf $5}')
 
-if [ $ARECORD_HOSTNAME != ${PTR_HOSTNAME%?} ]; then
+echo "$DNS_RETURN"
+echo "$PTR"
+
+if [ "$ARECORD_HOSTNAME" != "${PTR_HOSTNAME%?}" ]; then
   echo "Hostname in A record and PTR record do not match."
-  echo $ARECORD_HOSTNAME
-  echo $PTR_HOSTNAME
+  echo "$ARECORD_HOSTNAME"
+  echo "$PTR_HOSTNAME"
 else
   echo "Records match."
 fi
