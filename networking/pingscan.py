07e78bd89cb2a3fa31afd1768bfa7e0abadc397d
Gavin Purcell <mkorangestripe@gmail.com>
2022-03-14 10:00:19 -0500

    Update aliases

--- networking/pingscan.py
+++ networking/pingscan.py
@@ -3,10 +3,10 @@
 
 # Alias examples:
 # ln -s [PATH_TO_FILE]/pingscan.py ~/.bin/pingscan
+# Assuming only one Class C address:
 # alias inetC='{ ip addr || ifconfig; } 2> /dev/null | grep "inet 192"'
-# THIRD_OCT_C=$(inetC | awk -F. '{print $3}')  # assuming only one class C address
-# alias pingscan-27="~/.bin/pingscan -w 2 -c 192.168.$THIRD_OCT_C.0/27"
-# alias pingscan-24="~/.bin/pingscan -w 2 -c 192.168.$THIRD_OCT_C.0/24"
+# alias pingscan-27="~/.bin/pingscan -w 2 -c 192.168.$(inetC | awk -F. '{print $3}').0/27"
+# alias pingscan-24="~/.bin/pingscan -w 2 -c 192.168.$(inetC | awk -F. '{print $3}').0/24"
 
 """
 Scan a network by pinging a CIDR range in parallel.
